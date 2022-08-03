8957e0928fe5760e6a344e88dc1fb9c4110827d5
Nauman3S <naumanshakir3s@gmail.com>
2022-08-03 13:20:19 +0200

    updated github actions to add cargo fmt

--- demo/src/ecal_rs.rs
+++ demo/src/ecal_rs.rs
@@ -1,16 +1,14 @@
 /// topic: /kpns/test/ping
-#[derive(Clone, PartialEq, ::prost::Message)]
-#[derive(ecal::Message)]
+#[derive(Clone, PartialEq, ::prost::Message, ecal::Message)]
 #[type_prefix = "kpns_msgs."]
 pub struct Ping {
-    #[prost(uint64, tag="1")]
+    #[prost(uint64, tag = "1")]
     pub sync: u64,
 }
 /// topic: /kpns/test/pong
-#[derive(Clone, PartialEq, ::prost::Message)]
-#[derive(ecal::Message)]
+#[derive(Clone, PartialEq, ::prost::Message, ecal::Message)]
 #[type_prefix = "kpns_msgs."]
 pub struct Pong {
-    #[prost(uint64, tag="1")]
+    #[prost(uint64, tag = "1")]
     pub sync: u64,
 }
